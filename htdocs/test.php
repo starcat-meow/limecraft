51b400589109a0f05242bf49a75ac268a6d8c072
starcat-meow <flyusb@163.com>
2024-10-07 00:12:11 +0800

    指针碰到按钮变化

--- htdocs/test.php
+++ htdocs/test.php
@@ -2,10 +2,6 @@
 include "header.php";
 $message=new Message();
 $message->Add(1,1);
-$message->Add(2,1);
-$message->Add(3,1);
-$message->Add(4,1);
-$message->Add(5,1);
 $message->Remove(2);
 $message->GetNoRead();
 print_r($message->Select(2));
